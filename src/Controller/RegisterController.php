2f690b38d918a7bf5761f422dc32d19609f9b70f
Ackah Justin <ajustinmarie@gmail.com>
2023-04-23 14:18:26 +0200

    MON SITE V2 depart exo 21

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,15 +2,48 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\UserPassportInterface;
 
 class RegisterController extends AbstractController
 {
-    #[Route('/register', name: 'register')]
-    public function index(): Response
-    {
-        return $this->render('register/index.html.twig');
-    }
+
+        private $entityManager;
+
+        public function __construct(EntityManagerInterface $entityManager)
+        {
+            $this->entityManager = $entityManager;
+        }
+
+        #[Route('/inscription', name: 'inscription')]
+        public function index(Request $request, UserPasswordEncoderInterface $encoder): Response
+        {
+
+                $user = new User();
+                $form=$this->createForm(RegisterType::class, $user);        
+                $form->handleRequest($request);
+
+                if($form->isSubmitted() && $form->isValid()) 
+                {   
+                    $password_crypt=$encoder->encodePassword($user, $user->getPassword());
+                    $user->setPassword($password_crypt);
+
+                    $this->entityManager->persist($user);
+                    $this->entityManager->flush();
+                }
+
+                
+
+
+                    return $this->render('register/index.html.twig',[
+                        'form' => $form->createView()
+                    ]);
+        }
 }
